2ff3a8a272f80e1b212e18ae4ef366497796c565
Karyna Katrich <karyna.katrich@mhp.com>
2021-03-17 14:34:24 +0100

    Initial Commit

---
+++ Shared/UniversalSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniversalSwiftUIApp.swift
+//  Shared
+//
+//  Created by Karyna Katrich on 17.03.21.
+//
+
+import SwiftUI
+
+@main
+struct UniversalSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
