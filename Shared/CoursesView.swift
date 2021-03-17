879bbc644b76c81b56ab3a7949e8c8459cdf97eb
Karyna Katrich <karyna.katrich@mhp.com>
2021-03-17 15:50:33 +0100

    Implement List styles

---
+++ Shared/CoursesView.swift
@@ -0,0 +1,23 @@
+//
+//  CoursesView.swift
+//  UniversalSwiftUI
+//
+//  Created by Karyna Katrich on 17.03.21.
+//
+
+import SwiftUI
+
+struct CoursesView: View {
+    var body: some View {
+        List(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
+            CourseRow()
+        }
+        .listStyle(InsetGroupedListStyle())
+    }
+}
+
+struct CoursesView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoursesView()
+    }
+}
